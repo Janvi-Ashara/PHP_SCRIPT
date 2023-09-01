b8cac5e235ae4a7739dd3e0a8fab39c7a3b9791c
Janvi-Ashara <143256436+Janvi-Ashara@users.noreply.github.com>
2023-09-01 12:07:11 +0530

    Add files via upload
    php assignments

---
+++ script_10.php
@@ -0,0 +1,5 @@
+<?php
+$name = "hello,john!";
+$age = "you are 25 years old";
+echo "$name , $age";
+?>
---
+++ script_5.php
@@ -0,0 +1,12 @@
+
+<?php
+$num = "42";
+$text = "hello";
+echo "the num is:$num , the text is:$text";
+?>
+
+<?php
+$num = "The number is:42";
+$text = "the text is:hello!";
+echo $num, $text;
+?>
